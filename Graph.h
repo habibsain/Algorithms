c2ad25547ccaa7b3694c97964f5c369b888c73ff
habibsain <habibsain25@gmail.com>
2024-11-15 13:46:20 +0530

    revesring and transposing a graph

--- Graph.h
+++ Graph.h
@@ -5,9 +5,55 @@
 *Save the edgelist in the form of an adjointlist
 *
 */
-#Pragma once
+#pragma once
 
 #include<iostream>
 #include<vector>
 
-using
+using namespace std;
+
+class Graph{
+    vector<vector<int>> adjlist;
+    public:
+    Graph(int vertices){
+        adjlist.resize(vertices);
+    }
+    void addEdges(vector<vector<int>> edgelist){
+        for(auto edge: edgelist){
+            adjlist[edge[0]].push_back(edge[1]);
+        }
+    }
+    void addEdge(int u, int v){
+        adjlist[u].push_back(v);
+        
+    }
+    vector<int> getAdjVertices(int u){
+        return adjlist[u];
+    }
+
+    vector<vector<int>> getAdjList(){
+        return adjlist;
+    }
+
+    vector<vector<int>> reverseGraph(){
+        vector<vector<int>> revlist;
+        for(int i = 0; i < adjlist.size(); i++){
+            for(auto vertex: adjlist[i]){
+                vector<int> edge = {vertex, i};
+                revlist.push_back(edge);
+            }
+        }
+        return revlist;
+    }
+
+    vector<vector<int>> transposeGraph(){
+        vector<vector<int>> translist;
+        for(int i = 0; i < adjlist.size(); i++){
+            for(auto vertex: adjlist[i]){
+                translist[vertex].push_back(i);
+            }
+        }
+        return translist;
+    }
+
+};
--- test.cpp
+++ test.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
+#include<vector>
+#include"Graph.h"
 
 using namespace std;
 
+void printGraph(Graph &g){
+    vector<vector<int>> adjlist = g.getAdjList();
+    for(int i = 0; i < adjlist.size(); i++){
+        cout << i << ": ";
+        for(auto vertex: adjlist[i]){
+            cout << vertex << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    cout<<"hello world" << endl;
+    Graph g(5);
+    vector<vector<int>> edgelist = {{0,1},{1,2},{2,3},{3,4},{4,0}};
+    g.addEdges(edgelist);
+    g.addEdge(0,3);
+    printGraph(g);
     return 0;
 }
